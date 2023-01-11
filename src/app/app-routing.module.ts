7fef6a458705f02a01e615fca20450f23e9eb9b9
hamza.zaidi <hamza.zaidi@snipp.com>
2023-01-11 18:18:10 +0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'departments', component:EmployeeListComponent},
+  {path:'employees',component:EmployeeDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [EmployeeListComponent,EmployeeDetailComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestmoduleComponent } from './testmodule/testmodule.component';
 import { FormsModule } from '@angular/forms';
 import { ComponentIteractionTestComponent } from './component-iteraction-test/component-iteraction-test.component';
 import { PipesTestComponent } from './pipes-test/pipes-test.component';
 import { EmployeeServiceService } from './employee-service.service';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
@@ -17,8 +15,7 @@ import {HttpClientModule} from '@angular/common/http';
     TestmoduleComponent,
     ComponentIteractionTestComponent,
     PipesTestComponent,
-    EmployeeListComponent,
-    EmployeeDetailComponent
+    routingComponent
   ],
   imports: [
     BrowserModule,
