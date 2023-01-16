66a37d0e721b955c9a8204a3a9640ffe9145c7a7
Zohaib Jawaid <94359956+zohaib-jawaid-tplex@users.noreply.github.com>
2023-01-16 13:54:22 +0500

    Add paramMap observable instead of snapshot

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -8,14 +9,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DepartmentDetailComponent {
 
-  constructor(private _activatedRoute:ActivatedRoute){}
+  constructor(private _activatedRoute:ActivatedRoute, private _router: Router){}
   public departmentId: any; 
   ngOnInit()
   {
- 
-    let id = this._activatedRoute.snapshot.paramMap.get('id');
-    this.departmentId = id;
+
+    // let id = this._activatedRoute.snapshot.paramMap.get('id');
+    // this.departmentId = id;
+    this._activatedRoute.paramMap.subscribe((params: ParamMap) =>
+    {
+      let id = params.get('id');
+      this.departmentId = id;
+    });
   }
+  previous()
+  {
 
+    let previousId = parseInt(this.departmentId) - 1;
+    this._router.navigate(['/lstdepartments',previousId]);
+  }
+  next()
+  {
 
+    let nextId = parseInt(this.departmentId) + 1;
+    this._router.navigate(['/lstdepartments',nextId]);
+  }
 }
